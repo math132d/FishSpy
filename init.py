e782703a2f4ade1e92a8a5be0c36fe0b5e065db2
Mathias <majohha@gmail.com>
2020-03-17 15:44:15 +0100

    linting and settings

--- init.py
+++ init.py
@@ -1,8 +1,7 @@
-import cv2
-import numpy as np
-
 from os import path, listdir
 
+import cv2
+import numpy as np
 
 def mean_squared_error(path1, path2):
     image1 = cv2.imread(path1)
@@ -11,17 +10,17 @@ def mean_squared_error(path1, path2):
     err = np.sum((image1.astype("float")-image2.astype("float")) ** 2)
     return err / (image1.shape[0] * image2.shape[1])
 
-frames_path = path.abspath("./vid/frames/")
+FRAMES_PATH = path.abspath("./vid/frames/")
 
-frames_list = list( filter( #Include only entries that are files, not folders
-    lambda file: path.isfile(path.join(frames_path, file)), 
-    listdir(frames_path)
+FRAMES_LIST = list(filter( #Include only entries that are files, not folders
+    lambda file: path.isfile(path.join(FRAMES_PATH, file)),
+    listdir(FRAMES_PATH)
 ))
 
-print("Looking for frames in: " + frames_path)
+print("Looking for frames in: " + FRAMES_PATH)
 
-for idx in range(len(frames_list)-1):
+for idx in range(len(FRAMES_LIST)-1):
     mean_error = mean_squared_error(
-            path.join(frames_path, frames_list[idx]),
-            path.join(frames_path, frames_list[idx+1])
-        )
+        path.join(FRAMES_PATH, FRAMES_LIST[idx]),
+        path.join(FRAMES_PATH, FRAMES_LIST[idx+1])
+    )
