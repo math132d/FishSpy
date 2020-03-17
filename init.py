611b27cd2a38cc8ed084c3cb296e09907e2ed8d0
Mathias <majohha@gmail.com>
2020-03-17 16:36:44 +0100

    Plotting Mean Errors. Failed to 'Cluster'

--- init.py
+++ init.py
@@ -1,14 +1,17 @@
 from os import path, listdir
 
+#from sklearn.mixture import GaussianMixture
+
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 def mean_squared_error(path1, path2):
     image1 = cv2.imread(path1)
     image2 = cv2.imread(path2)
 
     err = np.sum((image1.astype("float")-image2.astype("float")) ** 2)
-    return err / (image1.shape[0] * image2.shape[1])
+    return err / (image1.shape[0] * image1.shape[1])
 
 FRAMES_PATH = path.abspath("./vid/frames/")
 
@@ -17,6 +20,10 @@ FRAMES_LIST = list(filter( #Include only entries that are files, not folders
     listdir(FRAMES_PATH)
 ))
 
+MSE_LIST = []
+
+#GM = GaussianMixture(n_components=1)
+
 print("Looking for frames in: " + FRAMES_PATH)
 
 for idx in range(len(FRAMES_LIST)-1):
@@ -24,3 +31,13 @@ for idx in range(len(FRAMES_LIST)-1):
         path.join(FRAMES_PATH, FRAMES_LIST[idx]),
         path.join(FRAMES_PATH, FRAMES_LIST[idx+1])
     )
+
+    #Append as 1-element arrays since GM expects a 2D array
+    MSE_LIST.append([mean_error])
+
+#GM.fit(MSE_LIST)
+#PROBA = GM.predict_proba(MSE_LIST)
+
+plt.figure(figsize=(8,6))
+plt.scatter(range(len(MSE_LIST)), MSE_LIST, marker="x")
+plt.show()
