9556e92c4f66e57526cfa8f725eb12e284127042
Mathias <majohha@gmail.com>
2020-04-21 13:09:06 +0200

    converting images to gray for MSE

--- utils.py
+++ utils.py
@@ -5,14 +5,14 @@ import numpy as np
 
 def files_from(folder):
     #Returns sorted list of files in the defined folder
-    return list(filter(
+    return sorted(list(filter(
         lambda file: path.isfile(path.join(folder, file)),
         listdir(folder)
-    )).sort()
+    )))
 
 def mean_squared_error(path1, path2):
-    image1 = cv2.imread(path1)
-    image2 = cv2.imread(path2)
+    image1 = cv2.cvtColor(cv2.imread(path1), cv2.COLOR_BGR2GRAY)
+    image2 = cv2.cvtColor(cv2.imread(path2), cv2.COLOR_BGR2GRAY)
 
     err = np.sum((image1.astype("float")-image2.astype("float")) ** 2)
     return err / (image1.shape[0] * image1.shape[1])
