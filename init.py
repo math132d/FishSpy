b76ed75c2ecf1ad514b101f685f1159948f7b991
Mathias <majohha@gmail.com>
2020-04-21 12:26:43 +0200

    Predicting probability of image being 'abnormal'

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 from os import path, listdir
 
-#from sklearn.mixture import GaussianMixture
+from sklearn.mixture import GaussianMixture
 
 import cv2
 import numpy as np
@@ -15,29 +15,45 @@ def mean_squared_error(path1, path2):
 
 FRAMES_PATH = path.abspath("./vid/frames/")
 
+print(FRAMES_PATH)
+
 FRAMES_LIST = list(filter( #Include only entries that are files, not folders
     lambda file: path.isfile(path.join(FRAMES_PATH, file)),
     listdir(FRAMES_PATH)
 ))
 
+#Sort frames to be in order (linux dont return files in logial order...)
+FRAMES_LIST.sort()
+
 MSE_LIST = []
 
-#GM = GaussianMixture(n_components=1)
+GM = GaussianMixture(n_components=2)
 
 print("Looking for frames in: " + FRAMES_PATH)
 
 for idx in range(len(FRAMES_LIST)-1):
     mean_error = mean_squared_error(
-        path.join(FRAMES_PATH, FRAMES_LIST[idx]),
-        path.join(FRAMES_PATH, FRAMES_LIST[idx+1])
+        path.join(FRAMES_PATH, FRAMES_LIST[idx-1]),
+        path.join(FRAMES_PATH, FRAMES_LIST[idx])
     )
 
     #Append as 1-element arrays since GM expects a 2D array
     MSE_LIST.append([mean_error])
 
-#GM.fit(MSE_LIST)
-#PROBA = GM.predict_proba(MSE_LIST)
+GM.fit(MSE_LIST)
+
+#MSE_LIST_PROBA = enumerate(GM.predict_proba(MSE_LIST)[:,1])
+
+# ANOMALTIES = sorted(
+#     MSE_LIST_PROBA,
+#     key=lambda x: x[1]
+# )[:10]
+
+# print(ANOMALTIES)
 
 plt.figure(figsize=(8,6))
-plt.scatter(range(len(MSE_LIST)), MSE_LIST, marker="x")
+#Plot MSE difference
+plt.scatter(range(len(MSE_LIST)), MSE_LIST,c=GM.predict_proba(MSE_LIST)[:,1], marker="x")
+#Plot probability
+#plt.scatter(range(len(MSE_LIST)), GM.predict_proba(MSE_LIST)[:,1], marker="x")
 plt.show()
