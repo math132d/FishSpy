012f36671066901a4d67140781d722bdac8eb8b3
Mathias <majohha@gmail.com>
2020-04-23 09:03:44 +0200

    Modified TimeSlice to be in Milliseconds

--- TimeSlice.py
+++ TimeSlice.py
@@ -2,14 +2,18 @@ from datetime import timedelta
 
 class TimeSlice:
     def __init__(self, start, duration):
+        #Start and duration are both in milliseconds
         self.start = start
         self.duration = duration
+
+    def __str__(self):
+        return self.get_timestamp()
 
     def end(self):
         return self.start + self.duration
 
     def get_timestamp(self):
-        start = timedelta(milliseconds=self.start * (1000/25))
-        end = timedelta(milliseconds=self.end() * (1000/25))
+        start = timedelta(milliseconds=self.start)
+        end = timedelta(milliseconds=self.end())
 
         return "[{} -> {}]".format(str(start), str(end))
