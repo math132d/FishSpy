81373d7c59ac0bd0870794fd19b4def5a5be0542
math132d <majohha@gmail.com>
2020-05-14 14:35:26 +0200

    set up mog for evaluation

--- mog.py
+++ mog.py
@@ -1,58 +1,64 @@
+from os import path
 import cv2
 import numpy as np
 import utils
 
 from contourtracker import ContourTracker
 
-path = './vid/small.mp4'
-bg_subtractor = cv2.createBackgroundSubtractorMOG2(history=750)
-contourtracker = ContourTracker(10, 3, 25)
+def get_tracked_contours(in_path, space_grouping_threshold=10, time_grouping_threshold=3, min_duration=10, history=750):
+    #Function returns a contourtracker object which contains a list of TimeCountours (tracked contours)
 
-framebuffer = cv2.VideoCapture(path)
+    contourtracker = ContourTracker(time_grouping_threshold, time_grouping_threshold, min_duration)
+    bg_subtractor = cv2.createBackgroundSubtractorMOG2(history=history)
+    framebuffer = cv2.VideoCapture(in_path)
+    kernel_sm = np.ones((3,3), np.uint8)
 
-kernel_sm = np.ones((3,3), np.uint8)
-kernel_lg = np.ones((6,6), np.uint8)
+    if not framebuffer.isOpened():
+        print('Failed to open video file')
 
-if not framebuffer.isOpened():
-    print('Failed to open video file')
+    frame=0
+    while framebuffer.isOpened():
+        frame += 1;
 
-frame=0
-ctr_img = None
+        _ret, curr_frame = framebuffer.read()
 
-while framebuffer.isOpened():
-    frame += 1;
+        if not _ret:
+            print('Reached end of file')
+            break
 
-    _ret, curr_frame = framebuffer.read()
+        fg = bg_subtractor.apply(curr_frame)
 
-    if not _ret:
-        print('Reached end of file')
-        break
+        fg = cv2.medianBlur(fg, 5)
+        _, fg = cv2.threshold(fg, 0, 255, cv2.THRESH_BINARY)
+        fg = cv2.dilate(fg, kernel_sm, iterations=4)
 
-    fg = bg_subtractor.apply(curr_frame)
+        contours, _ = cv2.findContours(fg, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE);
 
-    fg = cv2.medianBlur(fg, 5)
-    _, fg = cv2.threshold(fg, 0, 255, cv2.THRESH_BINARY)
-    fg = cv2.dilate(fg, kernel_sm, iterations=4)
+        for contour in contours:
+            contourtracker.add_contour(contour, frame)
 
-    contours, _ = cv2.findContours(fg, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE);
+    return contourtracker
 
-    for contour in contours:
-        contourtracker.add_contour(contour, frame)
+def draw_tracked_contours(in_path, out_path, contourtracker, scale=(1.0, 1.0)):
+    #in_path: path to a video
+    #contourtracker: object with pre tracked contours
+    #scale: A touple of (x, y) scales at which to draw the bounding boxes on the video (if the contourtracker tracked a different size video)
+    #takes a path to a video and contourtracker and draws the bounding box of the tracked contours on the video
 
-framebuffer = cv2.VideoCapture(path)
-frame=0
+    framebuffer = cv2.VideoCapture(in_path)
+    
+    frame=0
+    if not framebuffer.isOpened():
+        print('Failed to open video file again')
 
-if not framebuffer.isOpened():
-    print('Failed to open video file again')
+    while framebuffer.isOpened():
+        frame += 1;
+        _ret, curr_frame = framebuffer.read()
 
-while framebuffer.isOpened():
-    frame += 1;
-    _ret, curr_frame = framebuffer.read()
+        if not _ret:
+            print('Reached end of file')
+            break
 
-    if not _ret:
-        print('Reached end of file')
-        break
+        img = contourtracker.draw_countours(curr_frame, scale, frame)
 
-    img = contourtracker.draw_countours(curr_frame, frame)
-
-    cv2.imwrite(f'./vid/frames/frame_{frame}.png', curr_frame)
+        cv2.imwrite(path.join(out_path, f'frame_{frame}.png'), curr_frame)
